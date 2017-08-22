78aaba16190c3bad160a0b7f9db195cceafa15f0
Vladislav Khramov <khramovw@gmail.com>
2017-08-22 22:02:57 +0300

    first commit

---
+++ js/plugin.js
@@ -0,0 +1,13 @@
+;(function ($) {
+	var header = $('header');
+
+	$(window).on('scroll', function () {
+
+		if ($(this).scrollTop() > 20){
+			header.addClass('on-scroll');
+		} else {
+			header.removeClass('on-scroll');
+		}
+	})
+
+})(jQuery);
